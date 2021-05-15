f4dac530e27b117a0f9ea488baeb62840f99ee11
johnyworld <johnyworld@naver.com>
2021-05-15 16:36:27 +0900

    Install SASS dependencies

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -2,3 +2,9 @@ declare module "*.css" {
     const mapping: Record<string, string>;
     export default mapping;
 }
+
+declare module "*.scss" {
+    const mapping: Record<string, string>;
+    export default mapping;
+}
+
