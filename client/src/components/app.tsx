9c2143b18ddfa526d85a153a00f9389f5ece637f
johnyworld <johnyworld@naver.com>
2021-05-21 21:16:38 +0900

    Test dark/light mode

--- client/src/components/app.tsx
+++ client/src/components/app.tsx
@@ -11,30 +11,42 @@ import { useEffect, useState } from 'preact/hooks';
 
 const App: FunctionalComponent = () => {
 
-    const [ hello, setHello ] = useState('');
+  const [ hello, setHello ] = useState('');
 
-    useEffect(() => {
-        axios.get('/api/hello').then(res=> {
-            setHello(res.data);
-        });
-    }, []);
+  useEffect(() => {
+    axios.get('/api/hello').then(res=> {
+      setHello(res.data);
+    });
+  }, []);
 
-    return (
-        <div id="preact_root">
-            <div style={{ height: '100px' }}></div>
-            <p>d</p>
-            { hello &&
-                <h2>{hello}</h2>
-            }
-            <Header />
-            <Router>
-                <Route path="/" component={Home} />
-                <Route path="/profile/" component={Profile} user="me" />
-                <Route path="/profile/:user" component={Profile} />
-                <NotFoundPage default />
-            </Router>
-        </div>
-    );
+  const setMode = (mode:string) => () => {
+    document.documentElement.setAttribute("data-theme", mode);
+  }
+
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    console.log('Dark!');
+  } else {
+    console.log('Light!');
+  }
+
+  return (
+    <div id="preact_root">
+      <div style={{ height: '100px' }}></div>
+      <button onClick={setMode('dark')}>dark</button>
+      <button onClick={setMode('light')}>light</button>
+      <p>d</p>
+      { hello &&
+        <h2>{hello}</h2>
+      }
+      <Header />
+      <Router>
+        <Route path="/" component={Home} />
+        <Route path="/profile/" component={Profile} user="me" />
+        <Route path="/profile/:user" component={Profile} />
+        <NotFoundPage default />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
