94dbcaec0253940a72ddca0eb84793aa3f139604
johnyworld <johnyworld@naver.com>
2021-05-22 11:07:15 +0900

    Add User model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+class User(models.Model):
+  email = models.CharField(max_length=40)
+  password = models.CharField(max_length=40)
+  username = models.CharField(max_length=20)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  last_login = models.DateTimeField(auto_now=True)
