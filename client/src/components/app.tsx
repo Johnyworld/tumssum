4ad3153ecf86f1f0b2157d6a2cfcf7f1e08a9c6c
johnyworld <johnyworld@naver.com>
2021-05-24 16:06:01 +0900

    removing test code

--- client/src/components/app.tsx
+++ client/src/components/app.tsx
@@ -9,14 +9,13 @@ import useStore from '../utils/context/useStore';
 
 const App: FunctionalComponent = () => {
 
-  console.log('===== app', );
+  const { theme, handleChangeTheme } = useStore('mode');
 
   return (
     <div id="preact_root">
       <div style={{ height: '100px' }}></div>
+      <button onClick={handleChangeTheme}>{theme}</button>
       <Header />
-      <Acomp />
-      <Bcomp />
       <Router>
         <Route path="/" component={Home} />
         <Route path="/profile/" component={Profile} user="me" />
@@ -26,18 +25,5 @@ const App: FunctionalComponent = () => {
     </div>
   );
 };
-
-const Acomp = () => {
-  const { theme, handleChangeTheme } = useStore('mode');
-  return (
-    <button onClick={handleChangeTheme}>{theme}</button>
-  )
-}
-
-const Bcomp = () => {
-  return (
-    <button>Hello</button>
-  )
-}
 
 export default App;
