e550a8dd8336c18901806890f8dbd93a914b62e6
johnyworld <johnyworld@naver.com>
2021-06-02 11:15:18 +0900

    Add static root to urls

--- server/urls.py
+++ server/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from server import settings
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
 from . import views
 
 urlpatterns = [
@@ -22,4 +25,6 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('profile/', views.profile, name='profile'),
     path('', views.index, name='index'),
+    # url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.base.STATIC_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.base.STATIC_ROOT}),
 ]
