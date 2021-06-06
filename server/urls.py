19d957a697cf41fce87408a8569290049fcb672b
johnyworld <johnyworld@naver.com>
2021-06-06 21:16:39 +0900

    Move uwsgi files

--- server/urls.py
+++ server/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from django.views.static import serve
 from . import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
