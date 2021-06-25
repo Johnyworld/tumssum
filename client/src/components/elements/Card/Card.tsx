b1056181d715d4d5cf22917efad4ba65ba35b39f
johnyworld <johnyworld@naver.com>
2021-06-25 21:17:48 +0900

    Add Card component

---
+++ client/src/components/elements/Card/Card.tsx
@@ -0,0 +1,21 @@
+import { h, FunctionalComponent } from 'preact';
+import { JSXInternal } from 'preact/src/jsx';
+import './Card.scss';
+
+export interface CardProps extends JSXInternal.DOMAttributes<HTMLDivElement> {
+	class: string;
+	padding?: 'none' | 'small' | 'regular';
+}
+
+const Card: FunctionalComponent<CardProps> = ({ children, class: className, padding='regular' }) => {
+
+	const paddingClassName = padding === 'none' ? '' : `p-${padding}`
+
+	return (
+		<div class={`card ${className} ${paddingClassName} `}>
+			{children}
+		</div>
+	)
+}
+
+export default Card;
