9295de90f5a03de7525ac8878345b84e6c48326c
johnyworld <johnyworld@naver.com>
2021-06-26 11:54:04 +0900

    Add Input story

---
+++ client/src/components/elements/Input/Input.stories.tsx
@@ -0,0 +1,25 @@
+import { h } from 'preact';
+import { Story, Meta } from '@storybook/preact';
+import Input, { InputProps } from './Input';
+
+export default {
+  title: 'Elements/Input',
+  component: Input,
+  argTypes: {
+    onChange: { action: 'onChange' },
+    type: { control: { type: 'select', options: [ 'text', 'number', 'email', 'password' ]}},
+  },
+} as Meta;
+
+const Template: Story<InputProps> = (args) => <Input {...args} ></Input>;
+
+export const Primary = Template.bind({});
+Primary.args = {
+	name: 'name',
+	label: 'Name',
+	value: '',
+	placeholder: 'Please enter your name',
+	fluid: false,
+	required: false,
+	readOnly: false,
+};
