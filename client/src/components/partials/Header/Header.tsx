270973b322da907ff5244f6d5a4e8057b227b523
johnyworld <johnyworld@naver.com>
2021-07-01 23:40:39 +0900

    Add Header component

---
+++ client/src/components/partials/Header/Header.tsx
@@ -0,0 +1,16 @@
+import { h, FunctionalComponent } from 'preact';
+import './Header.scss';
+
+interface Props {
+
+}
+
+const Header: FunctionalComponent<Props> = ({  }) => {
+	return (
+		<header class='header'>
+			Hello Header
+		</header>
+	)
+}
+
+export default Header;
