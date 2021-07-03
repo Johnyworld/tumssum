7c8926666f881082d3a03c4df6fed9ac65c94840
johnyworld <johnyworld@naver.com>
2021-07-03 17:30:15 +0900

    Add color props to Icon component

--- client/src/components/items/IconText/IconText.tsx
+++ client/src/components/items/IconText/IconText.tsx
@@ -1,17 +1,18 @@
 import { h, FunctionalComponent } from 'preact';
-import { IconType } from 'types';
+import { Color, IconType } from 'types';
 import Icon from '~components/elements/Icon';
 
 export interface IconTextProps {
 	icon: IconType;
 	text: string;
+	color?: Color;
 }
 
-const IconText: FunctionalComponent<IconTextProps> = ({ icon, text }) => {
+const IconText: FunctionalComponent<IconTextProps> = ({ icon, text, color='pen' }) => {
 	return (
 		<div class='flex flex-inline flex-gap-small'>
-			<Icon as={icon} strokeWidth={2} />
-			<p class='t-fit f-medium fw-bold'>{text}</p>
+			<Icon color={color} as={icon} strokeWidth={2} />
+			<p class={'t-fit f-medium fw-bold' + ` c-${color}`}>{text}</p>
 		</div>
 	)
 }
