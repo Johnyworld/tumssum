5d7604f7716f2a051d4939542ad15cd7cc952248
johnyworld <johnyworld@naver.com>
2021-07-15 01:36:59 +0900

    Add more button border styles

--- client/src/components/elements/button/button.tsx
+++ client/src/components/elements/button/button.tsx
@@ -8,18 +8,19 @@ export interface ButtonProps extends DefaultProps, JSXInternal.DOMAttributes<HTM
   color?: Color;
   size?: 'small' | 'regular' | 'large';
   paddingX?: 'narrow' | 'normal' | 'wide';
+  border?: 'rounded' | 'squared';
   fluid?: boolean;
   disabled?: boolean;
   type?: 'button' | 'submit';
   onClick?: h.JSX.MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button: FunctionalComponent<ButtonProps> = ({ children, class: className, style, color='primary', size, paddingX, fluid, disabled, type, onClick }) => {
+const Button: FunctionalComponent<ButtonProps> = ({ children, class: className, style, color='primary', size, paddingX, border, fluid, disabled, type, onClick }) => {
 
   return (
     <button
       style={style}
-      class={getClassNames([ 'button', className, [fluid, 'button-fluid'], [disabled, 'button-disabled'], [size, `button-${size}`], [paddingX, `button-${paddingX}`], [color, `bgc-${color}`] ])}
+      class={getClassNames([ 'button', className, [border, `button-${border}`], [fluid, 'button-fluid'], [disabled, 'button-disabled'], [size, `button-${size}`], [paddingX, `button-${paddingX}`], [color, `bgc-${color}`] ])}
       children={children}
       disabled={disabled}
       type={type}
