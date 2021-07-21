6546815413bb65b1a538d7bd7c86549a6408f552
johnyworld <johnyworld@naver.com>
2021-07-21 22:49:53 +0900

    Add CreateAccountModal Patial component

---
+++ client/src/components/partials/CreateAccountModal/CreateAccountModal.tsx
@@ -0,0 +1,81 @@
+import { h, FunctionalComponent } from 'preact';
+import { useEffect, useRef, useState } from 'preact/hooks';
+import Button from '~components/elements/Button';
+import DatePicker from '~components/elements/DatePicker';
+import Input from '~components/elements/Input';
+import Selector from '~components/elements/Selector/Selector';
+import Modal from '~components/layouts/Modal';
+import useInput from '~hooks/useInput';
+
+interface CreateAccountModalProps {
+	isOpen: boolean;
+	onCreateAccount: (title: string, isIncome: boolean, amount: number, createDate: string) => void;
+	onClose: () => void;
+}
+
+const CreateAccountModal: FunctionalComponent<CreateAccountModalProps> = (props) => {
+	return !props.isOpen ? null : <CreateAccountModalRender {...props} />
+}
+	
+
+const CreateAccountModalRender: FunctionalComponent<CreateAccountModalProps> = ({ isOpen, onClose, onCreateAccount }) => {
+
+	const inputRef = useRef<HTMLInputElement>(null);
+	
+	const [title, handleChangeTitle] = useInput('');
+	const [amount, handleChangeAmount] = useInput('');
+	const [isIncome, setIsIncome] = useState(false);
+	const [createDate, _, setValue] = useInput(new Date().toISOString());
+
+
+	const handleChangeIsIncome = (id: string) => () => {
+		if (id === 'income') setIsIncome(true);
+		else setIsIncome(false);
+	}
+
+
+	const handleSubmit: h.JSX.GenericEventHandler<HTMLFormElement> = (e) => {
+		e.preventDefault();
+		onCreateAccount(title, isIncome, +amount, createDate);
+	}
+
+
+	useEffect(() => {
+		if (inputRef.current) {
+			inputRef.current.focus();
+		}
+	}, [inputRef.current]);
+
+
+	return (
+		<Modal isOpen={isOpen} onClose={onClose}>
+			<form onSubmit={handleSubmit}>
+				<Modal.Header heading='새 기록 추가하기' onClose={onClose} />
+				<Modal.Content padding class='gap-regular'>
+					<Selector
+						fluid
+						label='지출/수입'
+						selected={isIncome ? 'income' : 'expenditure'}
+						onChange={handleChangeIsIncome}
+						list={[
+							{ id: 'expenditure', text: '지출' },
+							{ id: 'income', text: '수입' },
+						]}
+					/>
+					<Input fluid required name='title' label='제목' placeholder='무엇을 지출/수입 하셨나요?' value={title} onChange={handleChangeTitle} inputRef={inputRef} />
+					<Input fluid required name='amount' label='금액' placeholder='금액을 입력하세요.' value={amount} onChange={handleChangeAmount} type='number' min={0} removeAutoComplete />
+					<div class='grid grid-col-2 grid-gap-regular'>
+						<DatePicker fluid label='날짜' date={createDate} onChange={(date) => setValue(date)} />
+						<DatePicker fluid label='날짜' date={createDate} onChange={(date) => setValue(date)} />
+					</div>
+					<div style={{ height: '30px' }} />
+				</Modal.Content>
+				<Modal.Footer>
+					<Button border='squared' fluid size='large' type='submit' children='확인' />
+				</Modal.Footer>
+			</form>
+		</Modal>
+	)
+}
+
+export default CreateAccountModal;
--- client/src/features/month/MonthlyCalendar/MonthlyCalendar.tsx
+++ client/src/features/month/MonthlyCalendar/MonthlyCalendar.tsx
@@ -2,16 +2,12 @@ import { h, FunctionalComponent } from 'preact';
 import { useEffect, useRef, useState } from 'preact/hooks';
 import { Account } from 'types';
 import Button from '~components/elements/Button';
-import DatePicker from '~components/elements/DatePicker';
-import Input from '~components/elements/Input';
-import Selector from '~components/elements/Selector/Selector';
 import Calendar from '~components/items/Calendar';
 import MonthSelector from '~components/items/MonthSelector';
 import NavigationMenu from '~components/items/NavigationMenu';
-import Modal from '~components/layouts/Modal';
+import CreateAccountModal from '~components/partials/CreateAccountModal';
 import useCalendar from '~hooks/useCalendar';
 import useFetch from '~hooks/useFetch';
-import useInput from '~hooks/useInput';
 import useList from '~hooks/useList';
 import useToggle from '~hooks/useToggle';
 import { useDispatch, useSelector } from '~utils/redux/hooks';
@@ -25,12 +21,7 @@ const MonthlyCalendar: FunctionalComponent = () => {
 	const dispatch = useDispatch();
 	const toggleCreateModal = useToggle();
 	const [selected, setSelected] = useState('calendar');
-	const [create, setCreate] = useState('minus');
 	const inputRef = useRef<HTMLInputElement>(null);
-	const [createDate, _, setValue] = useInput(new Date().toISOString());
-
-	const [title, handleChangeTitle] = useInput('');
-	const [amount, handleChangeAmount] = useInput('');
 
 	const { list, setList, handleUpdate, handleAdd } = useList<Account>([]);
 
@@ -58,14 +49,13 @@ const MonthlyCalendar: FunctionalComponent = () => {
 		onUpdate: handleUpdate,
 	});
 
-	const handleCreateAccount: h.JSX.GenericEventHandler<HTMLFormElement> = (e) => {
-		e.preventDefault();
+	const handleCreateAccount = (title: string, isIncome: boolean, amount: number, datetime: string) => {
 		if (createNewAccount.loading) return;
 		createNewAccount.call({
 			user_id: user!.id,
 			title,
-			account: create === 'minus' ? -amount : amount,
-			datetime: new Date().toISOString(),
+			account: isIncome ? amount : -amount,
+			datetime,
 		})
 	}
 
@@ -110,33 +100,11 @@ const MonthlyCalendar: FunctionalComponent = () => {
 				onDragging={handleDragging}
 			/>
 
-			<Modal isOpen={toggleCreateModal.checked} onClose={toggleCreateModal.handleOff} >
-				<form onSubmit={handleCreateAccount}>
-					<Modal.Header heading='새 기록 추가하기' onClose={toggleCreateModal.handleOff} />
-					<Modal.Content padding class='gap-regular'>
-						<Selector
-							fluid
-							label='지출/수입'
-							selected={create}
-							onChange={(id) => () => setCreate(id)}
-							list={[
-								{ id: 'minus', text: '지출' },
-								{ id: 'plus', text: '수입' },
-							]}
-						/>
-						<Input fluid required name='title' label='제목' placeholder='무엇을 지출/수입 하셨나요?' value={title} onChange={handleChangeTitle} inputRef={inputRef} />
-						<Input fluid required name='amount' label='금액' placeholder='금액을 입력하세요.' value={amount} onChange={handleChangeAmount} type='number' min={0} removeAutoComplete />
-						<div class='grid grid-col-2 grid-gap-regular'>
-							<DatePicker fluid label='날짜' date={createDate} onChange={(date) => setValue(date)} />
-							<DatePicker fluid label='날짜' date={createDate} onChange={(date) => setValue(date)} />
-						</div>
-						<div style={{ height: '30px' }} />
-					</Modal.Content>
-					<Modal.Footer>
-						<Button border='squared' fluid size='large' type='submit' children='확인' />
-					</Modal.Footer>
-				</form>
-			</Modal>
+			<CreateAccountModal
+				isOpen={toggleCreateModal.checked}
+				onClose={toggleCreateModal.handleOff}
+				onCreateAccount={handleCreateAccount}
+			/>
 
 		</div>
 	)
