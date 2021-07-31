5eede98d62618596e53f4ca6b262901b2a724960
johnyworld <johnyworld@naver.com>
2021-07-31 14:21:31 +0900

    AccountFormModal 에 필요 없는 코드 삭제

--- client/src/components/partials/AccountFormModal/AccountFormModal.tsx
+++ client/src/components/partials/AccountFormModal/AccountFormModal.tsx
@@ -1,4 +1,5 @@
 import { h, FunctionalComponent } from 'preact';
+import { useTranslation } from 'preact-i18next';
 import { useState } from 'preact/hooks';
 import { Account } from 'types';
 import Button from '~components/elements/Button';
@@ -20,19 +21,20 @@ export interface AccountFormModalProps {
 
 const AccountFormModal: FunctionalComponent<AccountFormModalProps> = ({ initialValues, onClose, onConfirm, onDelete }) => {
 
+	const { t } = useTranslation();
 	
-	// const [title, handleChangeTitle] = useInput(initialValues?.title || '');
+	const [ title, changeTitle ] = useContentEditable(initialValues?.title || '');
 	const [amount, ___, setAmmount] = useInput(initialValues?.account ? Math.abs(initialValues.account)+'' : '');
 	const [isIncome, setIsIncome] = useState(initialValues?.account ? !(initialValues.account < 0) : false);
 	const [date, _, setDate] = useInput(initialValues?.datetime || getLocalString());
 	const [time, __, setTime] = useInput(initialValues?.datetime.split('T')[1]?.substr(0,5) || '');
-
-	const [ title, changeTitle ] = useContentEditable(initialValues?.title || '');
 	const [ memo, changeMemo ] = useContentEditable(initialValues?.memo || '');
+
 
 	const handleChangeIsIncome = (value: boolean) => {
 		setIsIncome(!value);
 	}
+	
 
 	const handleSubmit: h.JSX.GenericEventHandler<HTMLFormElement> = (e) => {
 		e.preventDefault();
@@ -47,8 +49,6 @@ const AccountFormModal: FunctionalComponent<AccountFormModalProps> = ({ initialV
 	return (
 		<Modal.Container>
 			<form onSubmit={handleSubmit}>
-				{/* <Modal.Header children={!initialValues ? '새 기록 추가하기' : '뒤로가기'} onGoBack={onGoBack} />
-				<Modal.XButton onClose={onClose} /> */}
 				<Modal.Content padding class='gap-regular'>
 					<ContentEditable
 						value={title}
@@ -80,31 +80,11 @@ const AccountFormModal: FunctionalComponent<AccountFormModalProps> = ({ initialV
 							onChange={changeMemo}
 						/>
 					</div>
-					{/* <Selector
-						fluid
-						label='지출/수입'
-						selected={isIncome ? 'income' : 'expenditure'}
-						onChange={handleChangeIsIncome}
-						list={[
-							{ id: 'expenditure', text: '지출' },
-							{ id: 'income', text: '수입' },
-						]}
-					/>
-					<Input fluid required name='title' label='제목' maxLength={TITLE_MAX_LENGTH} placeholder='무엇을 지출/수입 하셨나요?' value={title} onChange={(e) => changeTitle(e.currentTarget.value)} />
-					<Input fluid required name='amount' label='금액' min={0} max={ACCOUNT_MAX} placeholder='금액을 입력하세요.' value={amount} onChange={handleChangeAmount} type='number' removeAutoComplete />
-					<div class='grid grid-col-2 grid-gap-regular'>
-						<DatePicker fluid styleType='input' label='날짜' date={date} onChange={(date) => setDate(date)} placeholder='비어있음' />
-						<TimePicker fluid styleType='input' label='시간' time={time} onChange={(date) => setTime(date)} placeholder='비어있음' />
-					</div>
-					<div style={{ height: '30px' }} /> */}
 				</Modal.Content>
 				<Modal.Footer flex padding>
 					{ onDelete && initialValues ? <p class='c-red f-bold pointer' onClick={onDelete(initialValues.id)}>삭제</p> : <p /> }
-					<Button type='submit' children={initialValues ? '저장' : '확인'} />
+					<Button type='submit' children={t('common_save')} />
 				</Modal.Footer>
-				{/* <Modal.Footer>
-					<Button border='squared' fluid size='large' type='submit' children='확인' />
-				</Modal.Footer> */}
 			</form>
 		</Modal.Container>
 	)
