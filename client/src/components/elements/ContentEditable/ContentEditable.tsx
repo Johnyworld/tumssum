c477bc1a9acee0bf4dbf910f064e91cb425cc1d1
johnyworld <johnyworld@naver.com>
2021-07-31 11:30:32 +0900

    ContentEditable 숫자 타입에 +/- 선택 버튼 추가

--- client/src/components/elements/ContentEditable/ContentEditable.tsx
+++ client/src/components/elements/ContentEditable/ContentEditable.tsx
@@ -55,6 +55,10 @@ const ContentEditable: FunctionalComponent<ContentEditableProps> = ({ class: cla
 		}
 	}
 
+	const handleChangeNumberNegative = () => {
+		onChangeNumberNegative && onChangeNumberNegative(!isNumberNegative);
+	}
+
 	const handleBlur: h.JSX.FocusEventHandler<HTMLDivElement> = (e) => {
 		if (type === 'number') {
 			const removeCharacters = ref.current.innerText.replace(/([^0-9])/gi, "");;
@@ -73,19 +77,46 @@ const ContentEditable: FunctionalComponent<ContentEditableProps> = ({ class: cla
 		}, [ref.current]);
 	}
 
+
+	const content = <div
+		class={classes}
+		style={style}
+		contentEditable
+		placeholder={placeholder}
+		ref={ref}
+		onInput={handleInput}
+		onKeyDown={handleKeyDown}
+		onBlur={handleBlur}
+		children={!ref.current && value ? value : ''}
+	/>
+
 	return (
-		<div
-			class={classes}
-			style={style}
-			contentEditable
-			placeholder={placeholder}
-			ref={ref}
-			onInput={handleInput}
-			onKeyDown={handleKeyDown}
-			onBlur={handleBlur}
-			children={!ref.current && value ? value : ''}
-		/>
+		!onChangeNumberNegative	
+			? content
+			: <div class='pos-relative fluid'>
+					{content}
+					<div class='content-editable-number-svg pos-center-y pointer' onClick={handleChangeNumberNegative}>
+						{ isNumberNegative
+							? <svg width="25" height="16" viewBox="0 0 25 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M0.500001 8C0.500001 3.85786 3.85787 0.500001 8 0.500001L16.5 0.500002L16.5 15.5L8 15.5C3.85786 15.5 0.5 12.1421 0.500001 8Z" stroke="var(--color-gray_strong)"/>
+									<rect x="4.99976" y="7.40196" width="6" height="1.2" fill="var(--color-gray)"/>
+									<rect x="8.59973" y="5.00196" width="6" height="1.2" transform="rotate(90 8.59973 5.00196)" fill="var(--color-gray)"/>
+									<circle cx="16.9998" cy="8.00196" r="8" fill="var(--color-gray_strong)"/>
+									<rect x="14" y="7.40186" width="6" height="1.2" fill="var(--color-paper)"/>
+								</svg>
+
+							: <svg width="25" height="16" viewBox="0 0 25 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<path d="M24.5 8C24.5 12.1421 21.1421 15.5 17 15.5H8.5V0.5H17C21.1421 0.5 24.5 3.85786 24.5 8Z" stroke="var(--color-gray_strong)"/>
+									<rect x="14" y="7.4" width="6" height="1.2" fill="var(--color-gray)"/>
+									<circle cx="8" cy="8" r="8" fill="var(--color-gray_strong)"/>
+									<rect x="5" y="7.4" width="6" height="1.2" fill="var(--color-paper)"/>
+									<rect x="8.59998" y="5" width="6" height="1.2" transform="rotate(90 8.59998 5)" fill="var(--color-paper)"/>
+								</svg>
+						}
+					</div>
+				</div>
 	)
 }
+
 
 export default ContentEditable;
