ba6bf1dcb929c7f5039816943bed35eef211086e
johnyworld <johnyworld@naver.com>
2021-09-20 17:12:15 +0900

    예산 데이터 찾을 때 무한 재귀 버그 수정

--- client/src/utils/budgetUtils.ts
+++ client/src/utils/budgetUtils.ts
@@ -2,22 +2,23 @@ import { Budget } from "types"
 import numberUtils from "./numberUtils";
 
 
-const findBudgetWithDate: (budgets: Budget[], currentDate: string) => Budget = (budgets, currentDate) => {
+const findBudgetWithDate: (budgets: Budget[], currentDate: string, count: number) => Budget | null = (budgets, currentDate, count) => {
 	const exists = budgets.find(budget => budget.date === currentDate);
 	if (exists) return exists;
+	if (count <= 0) return null;
 	else {
 		const split = currentDate.split('-');
 		const Y = +split[0];
 		const M = +split[1];
 		const date = `${M === 1 ? Y-1 : Y}-${M === 1 ? 12 : numberUtils.getZeroNumber(M-1)}`;
-		return findBudgetWithDate(budgets, date);
+		return findBudgetWithDate(budgets, date, count-1);
 	}
 }
 
 
 const getBudgetOfCategory: (category_id: number, budgets: Budget[], currentDate: string) => number | undefined = (category_id, budgets, currentDate) => {
 	const filtered = budgets.filter(budget => budget.category === category_id);
-	if (filtered.length) return findBudgetWithDate(filtered, currentDate).budget;
+	if (filtered.length) return findBudgetWithDate(filtered, currentDate, 12)?.budget;
 	else return undefined;
 }
 
