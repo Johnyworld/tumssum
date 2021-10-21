811307f884f24368e88232fa103695513431dbb4
johnyworld <johnyworld@naver.com>
2021-10-21 23:29:28 +0900

    Add useCallback and useMemo to useResizeSide.tsx

--- client/src/hooks/useResizeSide.ts
+++ client/src/hooks/useResizeSide.ts
@@ -1,5 +1,5 @@
 import { h } from 'preact';
-import { useRef, useState } from 'preact/hooks';
+import { useCallback, useRef, useState } from 'preact/hooks';
 import { SIDE_WIDTH_DEFAULT, SIDE_WIDTH_MAX, SIDE_WIDTH_MIN } from '~utils/constants';
 
 
@@ -13,23 +13,23 @@ export default () => {
 	const [ tempWidth, setTempWidth ] = useState<number>(0);
 	const [ grabPos, setGrabPos ] = useState<number | null>(null);
 
-	const handleBorderMouseDown: h.JSX.MouseEventHandler<HTMLDivElement> = (e) => {
+	const handleBorderMouseDown: h.JSX.MouseEventHandler<HTMLDivElement> = useCallback((e) => {
 		setGrabPos(e.clientX);
 		setTempWidth(sideWidth);
-	}
+	}, [sideWidth]);
 
-	const handleContainerMouseUp: h.JSX.MouseEventHandler<HTMLDivElement | HTMLElement> = (e) => {
+	const handleContainerMouseUp: h.JSX.MouseEventHandler<HTMLDivElement | HTMLElement> = useCallback((e) => {
 		localStorage.setItem('home_side', sideWidth+'');
 		setGrabPos(null);
-	}
+	}, [sideWidth]);
 
-	const handleContainerDrag: h.JSX.MouseEventHandler<HTMLElement> = (e) => {
+	const handleContainerDrag: h.JSX.MouseEventHandler<HTMLElement> = useCallback((e) => {
 		if (grabPos) {
 			const movePos = (grabPos - e.clientX);
 			const newWidth = tempWidth + movePos;
 			setSideWidth(newWidth > SIDE_WIDTH_MAX ? SIDE_WIDTH_MAX : newWidth < SIDE_WIDTH_MIN ? SIDE_WIDTH_MIN : newWidth);
 		}
-	}
+	}, []);
 
 	return {
 		borderRef,
