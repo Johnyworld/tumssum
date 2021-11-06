ca1a180734b1bd52c3406918dc9bad4a73babd1c
johnyworld <johnyworld@naver.com>
2021-11-06 14:56:43 +0900

    IconText 컴포넌트에 column direction 추가

--- client/src/components/molecules/IconText/IconText.stories.tsx
+++ client/src/components/molecules/IconText/IconText.stories.tsx
@@ -2,12 +2,15 @@ import { h } from 'preact';
 import { Story, Meta } from '@storybook/preact';
 import IconText, { IconTextProps } from './IconText';
 import { icons } from '~components/atoms/Icon/Icon';
+import { Color } from 'types';
 
 export default {
 	title: 'Items/IconText',
 	component: IconText,
 	argTypes: {
     icon: { control: { type: 'select', options: icons }},
+    direction: { control: { type: 'select', options: [ 'row', 'column' ] }},
+    color: { control: { type: 'select', options: [ 'primary', 'black', 'gray' ] as Color[] }},
 	}
 } as Meta;
 
--- client/src/components/molecules/IconText/IconText.tsx
+++ client/src/components/molecules/IconText/IconText.tsx
@@ -2,18 +2,20 @@ import { h, FunctionalComponent } from 'preact';
 import { Color, DefaultProps, IconType } from 'types';
 import Icon from '~components/atoms/Icon';
 import { c } from '~utils/classNames';
+import './IconText.scss';
 
 export interface IconTextProps extends DefaultProps {
 	icon: IconType;
 	text: string;
 	color?: Color;
+	direction?: 'row' | 'column';
 	isHideTextForMobile?: boolean;
 	onClick?: () => void;
 }
 
-const IconText: FunctionalComponent<IconTextProps> = ({ class: className, icon, text, color='pen', isHideTextForMobile, onClick }) => {
+const IconText: FunctionalComponent<IconTextProps> = ({ class: className, icon, text, color='pen', direction, isHideTextForMobile, onClick }) => {
 	return (
-		<div class={c( 'icon-text', 'flex', 'flex-start', 'flex-gap-small', className, [!!onClick, 'pointer'] )} onClick={onClick}>
+		<div class={c( 'icon-text', className, [!!onClick, 'pointer'], [direction==='column', 'icon-text--column'] )} onClick={onClick}>
 			<Icon class={'icon-text-icon'} color={color} as={icon} />
 			<p class={c('icon-text-p t-fit', ` c-${color}`, [isHideTextForMobile, 'hide-mobile'] )}>{text}</p>
 		</div>
