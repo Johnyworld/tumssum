3f71a4b23276a06745973aad10db203bd9a8e06a
johnyworld <johnyworld@naver.com>
2021-11-06 16:07:04 +0900

    iconText 컴포넌트 클래스명 변경 to BEM

--- client/src/components/molecules/IconText/IconText.tsx
+++ client/src/components/molecules/IconText/IconText.tsx
@@ -1,5 +1,5 @@
 import { h, FunctionalComponent } from 'preact';
-import { Color, DefaultProps, IconType } from 'types';
+import { Color, DefaultProps, IconType, Size } from 'types';
 import Icon from '~components/atoms/Icon';
 import { c } from '~utils/classNames';
 import './IconText.scss';
@@ -7,17 +7,19 @@ import './IconText.scss';
 export interface IconTextProps extends DefaultProps {
 	icon: IconType;
 	text: string;
+	textSize?: Size;
 	color?: Color;
 	direction?: 'row' | 'column';
+	bold?: boolean;
 	isHideTextForMobile?: boolean;
 	onClick?: () => void;
 }
 
-const IconText: FunctionalComponent<IconTextProps> = ({ class: className, icon, text, color='pen', direction, isHideTextForMobile, onClick }) => {
+const IconText: FunctionalComponent<IconTextProps> = ({ class: className, icon, text, textSize, color='pen', direction, bold, isHideTextForMobile, onClick }) => {
 	return (
-		<div class={c( 'icon-text', className, [!!onClick, 'pointer'], [direction==='column', 'icon-text--column'] )} onClick={onClick}>
-			<Icon class={'icon-text-icon'} color={color} as={icon} />
-			<p class={c('icon-text-p t-fit', ` c-${color}`, [isHideTextForMobile, 'hide-mobile'] )}>{text}</p>
+		<div class={c( 'icon-text', className, [!!textSize, `f-${textSize}`], [!!onClick, 'pointer'], [direction==='column', 'icon-text--column'] )} onClick={onClick}>
+			<Icon class={'icon-text__icon'} color={color} as={icon} strokeWidth={bold ? 2 : 1} />
+			<p class={c('icon-text__text t-fit', ` c-${color}`, [bold, 'f-bold'], [isHideTextForMobile, 'hide-mobile'] )}>{text}</p>
 		</div>
 	)
 }
