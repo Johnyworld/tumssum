73a976eadc2faedd6777de70e7f9991d099c3569
johnyworld <johnyworld@naver.com>
2021-11-07 14:46:51 +0900

    build.sh 파일 업데이트

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+sudo systemctl stop nginx uwsgi
 cd /home/ubuntu/tumssum/client && npm run build
 . /home/ubuntu/tumssum/venv/bin/activate
 cd /home/ubuntu/tumssum && python3 manage.py collectstatic --settings=server.settings.prod
+sudo systemctl start nginx uwsgi
