226318396df7c2c7ed007645a114e79c01469197
johnyworld <johnyworld@naver.com>
2021-12-17 19:33:01 +0900

    Update put account api for modify and send money

--- api/views/account/putAccount.py
+++ api/views/account/putAccount.py
@@ -24,10 +24,24 @@ def putAccount(request):
 
   accountData = get_object_or_404(Account, pk=account_id)
 
+  actionType = 'WRITE' if to_id == None else 'SEND' if bank_id != None else 'MODIFY'
+  bankMonthAccount = -account if actionType == 'SEND' else account
+  toMonthAccount = account
 
   if (to_id):
-    account = -account
-    new_to_id = checkAndCreateBank(user_id, to_id, datetime, -account, headers)
+    toMonth = Month.objects.get(bank_id=accountData.to, date=accountData.datetime[:7])
+    expenditureGap = account - accountData.account # (바뀐 후 Account.account - 바뀌기 전 Account.account)
+    # to_id만 삭제할 수 있는 경우는 없다.
+
+    if (to_id != accountData.to) or (datetime[:7] != accountData.datetime[:7]):
+      # to 를 변경하거나 Datetime 의 달을 변경하는 경우.
+      toMonth.expenditure = toMonth.expenditure - accountData.account # 기존 Month
+      new_to_id = checkAndCreateBank(user_id, to_id, datetime, toMonthAccount, headers) # 새로운 Month
+
+    else :
+      toMonth.expenditure = toMonth.expenditure + expenditureGap
+
+    toMonth.save()
 
   # ==== MONTH LINK START ====
   # Bank 를 변경하는 경우와 Datetime 을 변경하는 경우에는 Month를 다시 연결해야 한다. 
@@ -37,7 +51,7 @@ def putAccount(request):
 
     if (bank_id):
       # Bank ID 가 Request 에서 넘어왔다면, Bank를 새로 연결하는 것이다. Bank와 연결 된 새로운 Month를 생성한다.
-      new_month_id = checkAndCreateBank(user_id, bank_id, datetime, account, headers)
+      new_month_id = checkAndCreateBank(user_id, bank_id, datetime, bankMonthAccount, headers)
 
 
   else:
@@ -47,17 +61,16 @@ def putAccount(request):
 
     if (bank_id == None):
       # Bank 를 제거하는 경우: 기존 Month 데이터의 Expenditure 만 수정해주면 된다.
-      month.expenditure = month.expenditure - accountData.account # 기존 Month
+      month.expenditure = month.expenditure - bankMonthAccount # 기존 Month
       accountData.month_id = None
 
     elif (bank_id != accountData.bank_id) or (datetime[:7] != accountData.datetime[:7]):
       # Bank 를 변경하거나 Datetime 의 달을 변경하는 경우이다.
-      month.expenditure = month.expenditure - accountData.account # 기존 Month
-      new_month_id = checkAndCreateBank(user_id, bank_id, datetime, account, headers) # 새로운 Month
-      new_to_id = checkAndCreateBank(user_id, to_id, datetime, -account, headers) # 새로운 Month
+      month.expenditure = month.expenditure - bankMonthAccount # 기존 Month
+      new_month_id = checkAndCreateBank(user_id, bank_id, datetime, bankMonthAccount, headers) # 새로운 Month
 
     else: # 아무것도 하지 않거나 Account 만 변경하는 경우
-      month.expenditure = month.expenditure + expenditureGap
+      month.expenditure = month.expenditure - expenditureGap if actionType == 'SEND' else month.expenditure + expenditureGap
 
     month.save()
 
@@ -79,7 +92,6 @@ def putAccount(request):
       datetime[:7],
       headers,
     )
-
 
   for k in reqData:
     setattr(accountData, k, reqData[k])
