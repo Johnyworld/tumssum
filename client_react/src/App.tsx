d74cc0af860a1aad29074eb65ba99ca1a2348c75
johnyworld <johnyworld@naver.com>
2022-01-07 00:16:26 +0900

    Add redux-toolkit and modeSlice

--- client_react/src/App.tsx
+++ client_react/src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useDispatch, useSelector } from './hooks';
+import { changeTheme } from './stores/modeSlice';
 
 const App: React.FC = () => {
+
+  const theme = useSelector(state => state.mode.theme);
+  const dispatch = useDispatch();
+
+  const handleChangeTheme = () => {
+    dispatch(changeTheme());
+  }
+
   return (
     <div className="App">
+      <h1>{theme}</h1>
+      <button onClick={handleChangeTheme}>changeTheme</button>
     </div>
   );
 }
---
+++ client_react/src/stores/modeSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+type Theme = 'light' | 'dark'
+
+const getSystemTheme: () => Theme = () => {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    return 'dark';
+  } else {
+    return 'light';
+  }
+}
+
+const localTheme = localStorage.getItem('theme') as Theme | null;
+const defaultTheme = localTheme || getSystemTheme();
+document.documentElement.setAttribute("data-theme", defaultTheme);
+
+
+const initialState = {
+  theme: defaultTheme,
+  fullLoading: false,
+}
+
+export const modeSlice = createSlice({
+  name: 'mode',
+  initialState,
+  reducers: {
+    changeTheme: (state) => {
+      const newTheme = state.theme === 'dark' ? 'light' : 'dark';
+      localStorage.setItem('theme', newTheme);
+      document.documentElement.setAttribute("data-theme", newTheme);
+      state.theme = newTheme;
+    },
+    changeFullLoading: (state, {payload}: PayloadAction<boolean>) => {
+      state.fullLoading = payload;
+    }
+  }
+})
+
+export const { changeTheme, changeFullLoading } = modeSlice.actions;
+
+export default modeSlice.reducer;
