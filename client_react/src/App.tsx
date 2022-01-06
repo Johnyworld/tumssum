64ee698adbf372cf73548e5c06072870c1c6ef3c
johnyworld <johnyworld@naver.com>
2022-01-06 20:55:23 +0900

    Add button component for test

--- client_react/src/App.tsx
+++ client_react/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/atoms/Button';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ client_react/src/components/atoms/Button/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export interface ButtonProps {
+
+}
+
+const Button: React.FC<ButtonProps> = ({  }) => {
+  return (
+    <div>
+      Hello Button
+    </div>
+  )
+}
+
+export default Button;
