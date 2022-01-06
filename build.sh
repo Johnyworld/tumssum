35ea33b5ca7254ad5d45fedbf72deaed96fe1517
Ubuntu <ubuntu@ip-172-31-1-152.ap-northeast-2.compute.internal>
2022-01-06 11:05:18 +0000

    Update build.sh file

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
-sudo rm -rf /static
+#!/bin/bash
+
+
+echo "*** ⛔  Stopping NginX and uWSGI... ***"
+sudo systemctl stop uwsgi nginx
+
+echo "*** 🛠   Building a client app... ***"
 cd /home/ubuntu/tumssum/client && npm run build
+
+echo "*** 🗃   Collecting static files... ***"
+sudo rm -rf /static
 . /home/ubuntu/tumssum/server/venv/bin/activate
 cd /home/ubuntu/tumssum/server && python3 manage.py collectstatic --settings=server.settings.prod
+
+echo "*** 🚀  Starting NginX and uWSGI ***"
+sudo systemctl start uwsgi nginx
+
+echo "*** ✅  Success!! ***"
