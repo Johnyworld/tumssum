bebc09aec8f2fddef45724047b859b3bfe00dd2a
johnyworld <johnyworld@naver.com>
2022-01-07 20:35:37 +0900

    Add NumberInput component with test and storybook

---
+++ client_react/src/components/atoms/NumberInput/NumberInput.stories.tsx
@@ -0,0 +1,25 @@
+import { Story, Meta } from '@storybook/react';
+import NumberInput, { NumberInputProps } from './NumberInput';
+
+export default {
+	title: 'Atoms/NumberInput',
+	component: NumberInput,
+	argTypes: {
+	
+	}
+} as Meta;
+
+const Template: Story<NumberInputProps> = (args) => <NumberInput {...args} />
+
+export const Default = Template.bind({});
+Default.args = {
+	value: '',
+	min: 5,
+	max: 10,
+	label: 'Label',
+	placeholder: 'Enter number...',
+	fluid: false,
+	readOnly: false,
+	required: false,
+	disabled: false,
+};
