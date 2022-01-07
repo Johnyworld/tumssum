59aab073bea81d15287a02857a86019547f00fa4
johnyworld <johnyworld@naver.com>
2022-01-08 01:26:23 +0900

    Add sending email api transfer for test

--- client_react/src/App.tsx
+++ client_react/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useDispatch, useSelector } from './hooks';
 import { changeTheme } from './stores/modeSlice';
@@ -8,18 +8,41 @@ import LoginPage from '~/pages/login';
 import RegisterPage from '~/pages/register';
 import ConfirmPage from '~/pages/confirm';
 import { getAccounts } from './stores/accountSlice';
+import EmailInput from './components/molecules/EmailInput';
+import Button from './components/atoms/Button';
+import api from './utils/api';
 
 
 const AppLoggedIn: React.FC = () => {
 
+  const [value, setValue] = useState('');
+	const [isSent, setIsSent] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(0);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getAccounts());
   }, []);
 
+  const handleSend = async () => {
+    setIsSent(false);
+    setLoading(true);
+    try {
+      await api.auth.sendEmail(value)
+      setIsSent(true);
+    }
+    catch (err) { setError(err as number) }
+    finally { setLoading(false) }
+  }
+
   return (
     <div className='page-container'>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      {isSent && <p>Sent</p>}
+      <EmailInput label='이메일' name='email' value={value} onChange={setValue} />
+      <Button onClick={handleSend} />
       <Routes>
         <Route path='/' element={<IndexPage />} />
       </Routes>
--- client_react/src/utils/api/index.ts
+++ client_react/src/utils/api/index.ts
@@ -1,17 +1,28 @@
 import axios from "axios"
 import { Account, ApiResponse } from "types"
 
+type Res<T> = Promise<ApiResponse<T>>
+
 interface Api {
+	auth: {
+		sendEmail: (email: string) => Res<null>;
+	},
 	accounts: {
-		getList: () => Promise<ApiResponse<Account[]>>
+		getList: () => Res<Account[]>
 	}
 }
 
 export default {
-	accounts: {
-		getList: async () => {
-			try { return await axios.get('/api/accsounts/') }
+	auth: {
+		sendEmail: async (email) => {
+			try { return await axios.get('/api/login/send/', { params: { email }}) }
 			catch (err) { throw 98 }
 		}
+	},
+	accounts: {
+		getList: async () => {
+			try { return await axios.get('/api/accounts/') }
+			catch (err) { throw 98 }
+		},
 	}
 } as Api;
