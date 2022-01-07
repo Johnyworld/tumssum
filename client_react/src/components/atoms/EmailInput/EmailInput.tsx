5589cbea8d2d08cff51b1bd001a3b99b745585c4
johnyworld <johnyworld@naver.com>
2022-01-07 20:14:16 +0900

    Make name props required in EmailInput component

--- client_react/src/components/atoms/EmailInput/EmailInput.tsx
+++ client_react/src/components/atoms/EmailInput/EmailInput.tsx
@@ -4,8 +4,8 @@ import { c } from '~/utils/classNames';
 import './EmailInput.scss';
 
 export interface EmailInputProps extends DefaultProps {
+	name: string;
 	value: string;	
-	name?: string;
 	label?: string;
 	placeholder?: string;
 	fluid?: boolean;
@@ -21,7 +21,6 @@ const EmailInput: React.FC<EmailInputProps> = (props) => {
 	const { className, style, value, name, label, placeholder, fluid, readOnly, required, disabled, onChange } = props;
 
 	const inputProps = { value, placeholder, readOnly, required, disabled };
-	const inputName = name || 'email-input';
 
 	const handleChange: React.ChangeEventHandler<HTMLInputElement> = useCallback(e => {
 		onChange(e.target.value);
@@ -29,10 +28,10 @@ const EmailInput: React.FC<EmailInputProps> = (props) => {
 
 	return (
 		<div className={c('email-input', className, [fluid, '&--fluid'])}>
-			{ label && <label htmlFor={inputName} className='email-input__label'>{label}</label> }
+			{ label && <label htmlFor={name} className='email-input__label'>{label}</label> }
 			<input
 				{...inputProps}
-				name={inputName}
+				name={name}
 				className='email-input__box'
 				type='email'
 				style={style}
