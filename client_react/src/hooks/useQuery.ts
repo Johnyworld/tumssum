6294af10234363e49d72d677f2e7666e9e153f18
johnyworld <johnyworld@naver.com>
2022-01-11 00:01:45 +0900

    Add useQuery hook

---
+++ client_react/src/hooks/useQuery.ts
@@ -0,0 +1,8 @@
+import { useLocation } from "react-router-dom";
+
+/**
+ * useage) const id = useQuery('id');
+ */
+export default function useQuery(key: string) {
+  return new URLSearchParams(useLocation().search).get(key);
+}
