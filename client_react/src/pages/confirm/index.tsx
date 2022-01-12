27802067b2761a3e9e1d53baaf584e6b01c421c5
johnyworld <johnyworld@naver.com>
2022-01-12 21:57:10 +0900

    Add email confirm page

--- client_react/src/pages/confirm/index.tsx
+++ client_react/src/pages/confirm/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from '~/hooks';
+import useQuery from '~/hooks/useQuery';
+import { setUser } from '~/stores/userSlice';
 
 
 const ConfirmPage: React.FC = () => {
-	return (
-		<div>
-			Hello ConfirmPage
-		</div>
-	)
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const email = useQuery('email');
+	const token = useQuery('token');
+  useEffect(() => {
+    axios.post('/api/login/', { username: email, password: token }).then(res => {
+      if (res.data) dispatch(setUser(res.data));
+      navigate('/');
+    }).catch(res => {
+      navigate('/');
+    });
+  }, []);
+  return null;
 }
 
 export default ConfirmPage;
