8f60fa2cd272ad46ffc39cdd75146018d848b675
johnyworld <johnyworld@naver.com>
2022-02-09 10:44:28 +0900

    Add CustomLocalStorage class

--- client_react/src/stores/userSlice.ts
+++ client_react/src/stores/userSlice.ts
@@ -1,40 +1,36 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { User } from 'types';
+import { CustomLocalStorage } from '~/utils/CustomLocalStorage';
 
-
-const userInfo = localStorage.getItem('userInfo');
-
+const customLocalStorage = new CustomLocalStorage();
 
 interface UserState {
   userInfo: User | null;
 }
 
-
 const initialState: UserState = {
-  userInfo: userInfo ? JSON.parse(userInfo) : null,
-}
-
+  userInfo: customLocalStorage.getUserInfo(),
+};
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    setUser: (state, {payload}: PayloadAction<User>) => {
-      state.userInfo = payload; 
-      localStorage.setItem('userInfo', JSON.stringify(payload));
+    setUser: (state, { payload }: PayloadAction<User>) => {
+      state.userInfo = payload;
+      customLocalStorage.setUserInfo(payload);
     },
-    updateUser: (state, {payload}: PayloadAction<User>) => {
-      const userData = {...state.userInfo, ...payload};
+    updateUser: (state, { payload }: PayloadAction<User>) => {
+      const userData = { ...state.userInfo, ...payload };
       state.userInfo = userData;
-      localStorage.setItem('userInfo', JSON.stringify(userData));
+      customLocalStorage.setUserInfo(userData);
     },
-    logout: (state) => {
+    logout: state => {
       state.userInfo = null;
-      localStorage.removeItem('userInfo');
-    }
+      customLocalStorage.removeUserInfo();
+    },
   },
 });
-
 
 export const { setUser, updateUser, logout } = userSlice.actions;
 
---
+++ client_react/src/utils/CustomLocalStorage.ts
@@ -0,0 +1,24 @@
+import { User } from 'types';
+
+const USER_INFO = 'tmsmusrinfo';
+
+export class CustomLocalStorage {
+  static instance: any;
+  constructor() {
+    if (CustomLocalStorage.instance) return CustomLocalStorage.instance;
+    CustomLocalStorage.instance = this;
+  }
+
+  getUserInfo(): User | null {
+    const item = localStorage.getItem(USER_INFO);
+    return item ? (JSON.parse(item) as User) : null;
+  }
+
+  setUserInfo(data: User): void {
+    localStorage.setItem(USER_INFO, JSON.stringify(data));
+  }
+
+  removeUserInfo(): void {
+    localStorage.removeItem(USER_INFO);
+  }
+}
