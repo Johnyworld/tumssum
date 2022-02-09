0747ce47948ba199472bf60b0786690d3ddc46db
johnyworld <johnyworld@naver.com>
2022-02-09 16:18:57 +0900

    Add getZeroNumber of numberUtil with test

--- client_react/src/utils/numberUtil/numberUtil.test.ts
+++ client_react/src/utils/numberUtil/numberUtil.test.ts
@@ -1,15 +1,21 @@
 import numberUtil from '.';
 
 test('returns numbers with comma as string type', () => {
-	expect(numberUtil.getComma(1234567)).toEqual('1,234,567');
-	expect(numberUtil.getComma(0)).toEqual('0');
-	expect(numberUtil.getComma(undefined!)).toEqual('');
-	expect(numberUtil.getComma(null!)).toEqual('');
-	expect(numberUtil.getComma(NaN!)).toEqual('');
+  expect(numberUtil.getComma(1234567)).toEqual('1,234,567');
+  expect(numberUtil.getComma(0)).toEqual('0');
+  expect(numberUtil.getComma(undefined!)).toEqual('');
+  expect(numberUtil.getComma(null!)).toEqual('');
+  expect(numberUtil.getComma(NaN!)).toEqual('');
 });
 
 test('returns number without characters as string type', () => {
-	expect(numberUtil.removeCharacters('123가나다456ABC789def')).toEqual('123456789');
-	expect(numberUtil.removeCharacters('abcdef')).toEqual('');
-	expect(numberUtil.removeCharacters('')).toEqual('');
+  expect(numberUtil.removeCharacters('123가나다456ABC789def')).toEqual('123456789');
+  expect(numberUtil.removeCharacters('abcdef')).toEqual('');
+  expect(numberUtil.removeCharacters('')).toEqual('');
+});
+
+test('returns number with zero as string type', () => {
+  expect(numberUtil.getZeroNumber(1)).toEqual('01');
+  expect(numberUtil.getZeroNumber(10)).toEqual('10');
+  expect(numberUtil.getZeroNumber(5)).toEqual('05');
 });
--- client_react/src/utils/numberUtil/numberUtil.ts
+++ client_react/src/utils/numberUtil/numberUtil.ts
@@ -1,9 +1,12 @@
-const getComma = (num: number | string) => !num && num !== 0 ? '' : num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-const removeCharacters = (num: string) => num.replace(/([^0-9])/gi, "") || '';
+const getComma = (num: number | string) =>
+  !num && num !== 0 ? '' : num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+const removeCharacters = (num: string) => num.replace(/([^0-9])/gi, '') || '';
+const getZeroNumber = (value: number) => (value < 10 ? '0' : '') + value;
 
 const numberUtil = {
-	getComma,
-	removeCharacters,
-}
+  getComma,
+  removeCharacters,
+  getZeroNumber,
+};
 
 export default numberUtil;
