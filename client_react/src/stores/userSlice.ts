ee060f24d76e0ba3642e8c7221a5c1a27483bafe
johnyworld <johnyworld@naver.com>
2022-02-10 11:30:49 +0900

    Move CustomLocalStorage file location

--- client_react/src/stores/userSlice.ts
+++ client_react/src/stores/userSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { User } from 'types';
-import { CustomLocalStorage } from '~/utils/CustomLocalStorage';
+import CustomLocalStorage from '~/utils/CustomLocalStorage';
 
 const customLocalStorage = new CustomLocalStorage();
 
