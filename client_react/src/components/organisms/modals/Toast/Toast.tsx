3870d2562ba9542c82e7ae95132afe418c04f5f6
johnyworld <johnyworld@naver.com>
2022-02-14 15:19:12 +0900

    Fix some error in Toast.tsx

--- client_react/src/components/organisms/modals/Toast/Toast.tsx
+++ client_react/src/components/organisms/modals/Toast/Toast.tsx
@@ -4,10 +4,9 @@ import { ToastItem } from '~/stores/toastSlice';
 import { c } from '~/utils/classNames';
 import './Toast.scss';
 
-
 export interface ToastProps {
-	toast: ToastItem;
-	onRemove: (index: number) => void;
+  toast: ToastItem;
+  onRemove: (index: number) => void;
 }
 
 const TIMER = 5000;
@@ -16,28 +15,30 @@ const TOAST_GAP = 8;
 const TOAST_MARGIN = 16;
 
 const Toast: React.FC<ToastProps> = ({ toast, onRemove }) => {
+  const { id, index, message, color } = toast;
 
-	const { id, index, message, color } = toast;
+  const [count, setCount] = useState(TIMER);
+  const [fadeout, setFadeout] = useState(false);
 
-	const [count, setCount] = useState(TIMER);
-	const [fadeout, setFadeout] = useState(false);
+  useEffect(() => {
+    if (count < 500 && !fadeout) setFadeout(true);
+    const interval = setInterval(() => setCount(count - 500), 500);
+    if (count < 0) {
+      onRemove(id);
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [count, fadeout, id, onRemove]);
 
-	useEffect(() => {
-		if (count < 500 && !fadeout) setFadeout(true);
-		const interval = setInterval(() => setCount(count-500), 500);
-		if (count < 0) {
-			onRemove(id);
-			clearInterval(interval);
-		}
-		return () => clearInterval(interval);
-	}, [count]);
-
-	return (
-		<div className={c( 'toast', `bgc-${color}`, [fadeout, '&--fadeout'])} style={{ top: index*(TOAST_HEIGHT + TOAST_GAP) + TOAST_MARGIN }}>
-			<p className='toast__message'>{message}</p>
-			<Icon as='x' size='small' color='white' strokeWidth={1.5} onClick={() => onRemove(id)} />
-		</div>
-	)
-}
+  return (
+    <div
+      className={c('toast', `bgc-${color}`, [fadeout, '&--fadeout'])}
+      style={{ top: index * (TOAST_HEIGHT + TOAST_GAP) + TOAST_MARGIN }}
+    >
+      <p className='toast__message'>{message}</p>
+      <Icon as='x' size='small' color='white' strokeWidth='bold' onClick={() => onRemove(id)} />
+    </div>
+  );
+};
 
 export default Toast;
