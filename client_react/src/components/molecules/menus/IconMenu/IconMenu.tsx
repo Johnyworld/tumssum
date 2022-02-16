ed8fdf7aaf345ea258e182065d2482818777b55d
johnyworld <johnyworld@naver.com>
2022-02-16 09:27:39 +0900

    Add selected and onSelect props to IconMenu component

--- client_react/src/components/molecules/menus/IconMenu/IconMenu.tsx
+++ client_react/src/components/molecules/menus/IconMenu/IconMenu.tsx
@@ -6,14 +6,21 @@ import './IconMenu.scss';
 
 export interface IconMenuProps {
   list: IconMenuItemType[];
+  selected?: string;
+  onSelect?: (id: string) => void;
 }
 
-const IconMenu: React.FC<IconMenuProps> = ({ list }) => {
+const IconMenu: React.FC<IconMenuProps> = ({ list, selected, onSelect }) => {
   return (
     <ul className='icon-menu'>
       {list.map(item => (
         <LinkTo key={item.id} to={item.href}>
-          <IconMenuItem icon={item.icon} text={item.text} />
+          <IconMenuItem
+            icon={item.icon}
+            text={item.text}
+            isSelected={selected === item.id}
+            onClick={onSelect ? () => onSelect(item.id) : undefined}
+          />
         </LinkTo>
       ))}
     </ul>
--- client_react/src/components/molecules/menus/IconMenuItem/IconMenuItem.tsx
+++ client_react/src/components/molecules/menus/IconMenuItem/IconMenuItem.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { IconType } from 'types';
 import Icon from '~/components/atoms/Icon';
+import { c } from '~/utils/classNames';
 import './IconMenuItem.scss';
 
 export interface IconMenuItemProps {
   icon: IconType;
   text: string;
+  isSelected?: boolean;
+  onClick?: () => void;
 }
 
-const IconMenuItem: React.FC<IconMenuItemProps> = ({ icon, text }) => {
+const IconMenuItem: React.FC<IconMenuItemProps> = ({ icon, text, isSelected, onClick }) => {
   return (
-    <li className='icon-menu-item'>
-      <Icon strokeWidth='bold' as={icon} />
-      <p className='icon-menu-item__text'>{text}</p>
+    <li className='icon-menu-item' onClick={onClick}>
+      <Icon strokeWidth='bold' as={icon} color={isSelected ? 'pen' : 'gray'} />
+      <p className={c('icon-menu-item__text', [isSelected, '&--selected'])}>{text}</p>
     </li>
   );
 };
