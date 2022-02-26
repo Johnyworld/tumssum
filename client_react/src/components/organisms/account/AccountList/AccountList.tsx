b32fd626070b21c9a9afc3b06921778dd771888b
johnyworld <johnyworld@naver.com>
2022-02-26 11:30:19 +0900

    Add x-scrolling to table of account-list

--- client_react/src/components/organisms/account/AccountList/AccountList.tsx
+++ client_react/src/components/organisms/account/AccountList/AccountList.tsx
@@ -12,31 +12,33 @@ export interface AccountListProps {
 
 const AccountList: React.FC<AccountListProps> = ({ accounts, categoryTree, bankTree, onChange }) => {
   return (
-    <table className='account-list'>
-      <thead>
-        <tr>
-          <th>번호</th>
-          <th>날짜</th>
-          <th>시간</th>
-          <th className='t-left'>제목</th>
-          <th className='t-right'>금액</th>
-          <th>카테고리</th>
-          <th>뱅크</th>
-        </tr>
-      </thead>
-      <tbody>
-        {accounts.map((account, i) => (
-          <AccountListItem
-            key={account.id}
-            index={i}
-            account={account}
-            categoryTree={categoryTree}
-            bankTree={bankTree}
-            onChange={onChange}
-          />
-        ))}
-      </tbody>
-    </table>
+    <div className='account-list'>
+      <table>
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th>날짜</th>
+            <th>시간</th>
+            <th className='t-left'>제목</th>
+            <th className='t-right'>금액</th>
+            <th>카테고리</th>
+            <th>뱅크</th>
+          </tr>
+        </thead>
+        <tbody>
+          {accounts.map((account, i) => (
+            <AccountListItem
+              key={account.id}
+              index={i}
+              account={account}
+              categoryTree={categoryTree}
+              bankTree={bankTree}
+              onChange={onChange}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
