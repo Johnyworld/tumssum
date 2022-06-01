c13b383bba0746986d4f1f10502e46da2e083d6e
johnyworld <johnyworld@naver.com>
2022-06-01 14:50:14 +0900

    Add: tests for CustomDate class

--- client_react/src/utils/CustomDate/CustomDate.test.ts
+++ client_react/src/utils/CustomDate/CustomDate.test.ts
@@ -14,6 +14,25 @@ test('returns local year and month', () => {
   expect(customDate.getLocalYearMonth()).toEqual('2022-02');
 });
 
+test('returns local time', () => {
+  expect(customDate.getLocalTime()).toEqual('07:30:55');
+});
+
+test('returns local hours and minutes', () => {
+  expect(customDate.getLocalHoursMinutes()).toEqual('07:30');
+});
+
 test('returns local string', () => {
   expect(customDate.getLocalString()).toEqual('1 Feb 2022');
+});
+
+test('returns local string year and month', () => {
+  expect(customDate.getLocalStringYearAndMonth()).toEqual('Feb 2022');
+});
+
+test('set month', () => {
+  customDate.setMonth(2);
+  expect(customDate.getLocalYearMonth()).toEqual('2022-04');
+  customDate.setMonth(-3);
+  expect(customDate.getLocalStringYearAndMonth()).toEqual('Jan 2022');
 });
